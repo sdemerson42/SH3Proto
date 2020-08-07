abf105ec598ffc9e6c2e61f0983b4b8c40e0bc15
SDE <sdemerson42@gmail.com>
2020-08-06 17:32:58 -0700

    Fixed a bug in CheckRoomFit() within the SceneBuilder that was causing rooms to occasionally overlap. Added a few more test chambers to the ForestRooms group.

--- SH3_Proto/Assets/Scripts/SceneBuilder.cs
+++ SH3_Proto/Assets/Scripts/SceneBuilder.cs
@@ -221,6 +221,8 @@ public class SceneBuilder : MonoBehaviour
             {
                 var tile = gridLayers[0].GetTile(new Vector3Int(i, j, 0));
                 if (tile != null) return false;
+                tile = gridLayers[1].GetTile(new Vector3Int(i, j, 0));
+                if (tile != null) return false;
             }
         }
 
