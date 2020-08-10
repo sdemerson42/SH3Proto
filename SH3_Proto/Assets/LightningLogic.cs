675c2738905ec204170d0848b1a5ea33bcc616dd
SDE <sdemerson42@gmail.com>
2020-08-10 14:18:15 -0700

    Added test skeleton and a lightning effect just for fun. Collisions seem to be working fine.

---
+++ SH3_Proto/Assets/LightningLogic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightningLogic : MonoBehaviour
+{
+
+    Light2D m_light;
+    int m_flashCounter = 0;
+    int m_flashTime = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_light = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (m_flashCounter > 0)
+        {
+            Flash();
+            return;
+        }
+
+        if (Random.Range(0, 300) == 0)
+        {
+            m_flashCounter = 1;
+            m_flashTime = Random.Range(3, 15);
+            m_light.enabled = true;
+        }
+    }
+
+    void Flash()
+    {
+        if (++m_flashCounter == m_flashTime)
+        {
+            m_flashCounter = 0;
+            m_light.enabled = false;
+        }
+
+        float intensity = Random.Range(0.5f, 0.65f);
+        m_light.intensity = intensity;
+    }
+}
---
+++ SH3_Proto/Assets/Scripts/Enemy/SkeletonLogic.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonLogic : MonoBehaviour
+{
+
+    Rigidbody2D m_rigidBody;
+
+    int m_moveCounter = 0;
+    int m_moveCoolDown = 0;
+    char m_dir = '-';
+    Vector2 m_velocity = new Vector2();
+    float m_speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_rigidBody = GetComponent<Rigidbody2D>();
+        SelectNewDirection();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MakeMove();
+    }
+
+    private void MakeMove()
+    {
+        m_moveCounter = (m_moveCounter + 1) % m_moveCoolDown;
+        if (m_moveCounter == 0) SelectNewDirection();
+
+        m_rigidBody.velocity = m_velocity;
+    }
+
+    void SelectNewDirection()
+    {
+        if (m_dir == 'n' || m_dir == 's')
+            m_dir = Random.Range(0, 2) == 0 ? 'w' : 'e';
+        else if (m_dir == 'w' || m_dir == 'e')
+            m_dir = Random.Range(0, 2) == 0 ? 'n' : 's';
+        else
+        {
+            switch(Random.Range(0, 4))
+            {
+                case 0:
+                    m_dir = 'n';
+                    break;
+                case 1:
+                    m_dir = 's';
+                    break;
+                case 2:
+                    m_dir = 'w';
+                    break;
+                case 3:
+                    m_dir = 'e';
+                    break;
+            }
+        }
+
+        m_velocity.x = 0f;
+        m_velocity.y = 0f;
+
+        switch(m_dir)
+        {
+            case 'n':
+                m_velocity.y = m_speed;
+                break;
+            case 's':
+                m_velocity.y = -1f * m_speed;
+                break;
+            case 'w':
+                m_velocity.x = -1f * m_speed;
+                break;
+            case 'e':
+                m_velocity.x = m_speed;
+                break;
+        }
+
+        m_moveCounter = 0;
+        m_moveCoolDown = Random.Range(20, 51);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerProjectile")
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        SelectNewDirection();
+    }
+}
