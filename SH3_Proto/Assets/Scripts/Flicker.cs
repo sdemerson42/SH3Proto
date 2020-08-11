538f3a43ce9dddd37eae389f461ecd9575adac15
SDE <sdemerson42@gmail.com>
2020-08-11 12:09:15 -0700

    Added a generic flicker script for 2D lights that should continually change intensity. Added to Braziers.

---
+++ SH3_Proto/Assets/Scripts/Flicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Flicker : MonoBehaviour
+{
+    public float intensityVariance = .5f;
+    public int changeFrames = 5; 
+
+    float m_startingIntensity;
+    Light2D m_light;
+    int m_changeCounter = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_light = GetComponent<Light2D>();
+        m_startingIntensity = m_light.intensity;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_changeCounter = ++m_changeCounter % changeFrames;
+
+        if (m_changeCounter == 0)
+        {
+            float intensity = Random.Range(m_startingIntensity - intensityVariance,
+                m_startingIntensity + intensityVariance);
+            m_light.intensity = intensity;
+        }
+    }
+}
